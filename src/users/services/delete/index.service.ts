0b968eabc1936ca860f4878183606c849cb1862b
Diego Garcia <dgarciavallejos@gmail.com>
2022-04-09 21:08:46 -0400

    add try/catch in the functions

--- src/users/services/delete/index.service.ts
+++ src/users/services/delete/index.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { ReturnModelType } from '@typegoose/typegoose';
+import { ApolloError } from 'apollo-server-express';
 import { InjectModel } from 'nestjs-typegoose';
 
 import { CurrentUser, Users } from 'src/users/entities/user.entity';
@@ -12,10 +13,16 @@ export class DeleteUsersService {
   ) {}
 
   async delete(id: string, currentUser: CurrentUser): Promise<Users> {
-    return this.userModel.findByIdAndUpdate(
-      id,
-      { $set: { deletedBy: currentUser.id as any } },
-      { new: true },
-    );
+    try {
+      return this.userModel.findByIdAndUpdate(
+        id,
+        { $set: { deletedBy: currentUser.id, updatedAt: new Date() } },
+        { new: true },
+      );
+    } catch {
+      throw new ApolloError('Error al eliminar el usuario', null, {
+        describe: 'Por favor verifique los datos ingresados',
+      });
+    }
   }
 }
