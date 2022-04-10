2707e29025e33fdf5ecdca1dc3d6ee9a12f1c2ff
Diego Garcia <dgarciavallejos@gmail.com>
2022-04-09 21:05:14 -0400

    update or create user create with google

--- src/auth/services/findUserOrSingUp/index.service.ts
+++ src/auth/services/findUserOrSingUp/index.service.ts
@@ -4,19 +4,29 @@ import { CreateUserInput } from 'src/users/dto/create-user.input';
 import { Users } from 'src/users/entities/user.entity';
 import { CreateUserService } from 'src/users/services/create/index.service';
 import { FindOneUserByEmailService } from 'src/users/services/findByEmail/index.service';
+import { UpdateUserService } from 'src/users/services/update/index.service';
 
 @Injectable()
 export class FindUserOrSignUpAuthService {
   constructor(
     private readonly findOneUsersService: FindOneUserByEmailService,
-    private readonly createUsersService: CreateUserService,
+    private readonly updateUserSerice: UpdateUserService,
+    private readonly createUserSerice: CreateUserService,
   ) {}
 
   async findUserOrSignUp(userInput: CreateUserInput): Promise<Users> {
     const user = await this.findOneUsersService.findOneByEmail(userInput.email);
 
-    if (!user) return await this.createUsersService.create(userInput);
+    if (!user) return await this.createUserSerice.create(userInput);
 
-    return user;
+    if (user)
+      return await this.updateUserSerice.update(
+        user.id,
+        {
+          $set: { googleId: userInput.googleId },
+        },
+        {},
+        null,
+      );
   }
 }
